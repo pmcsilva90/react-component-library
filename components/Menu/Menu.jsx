34d56920205f96a8bb40c22e348bc7a77e74e635
pmcsilva90 <pmcsilva90@hotmail.com>
2026-04-27 15:31:19 +0100

    fix useToggle import

--- components/Menu/Menu.jsx
+++ components/Menu/Menu.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import useToggle from "../../hooks/useToggle"
+import useToggle from "../../hooks/useToggle.js"
 
 const MenuContext = React.createContext()
 export { MenuContext }
